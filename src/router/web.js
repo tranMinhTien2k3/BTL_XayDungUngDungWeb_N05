57037b3f0952a05ed7a48a78bc45ef4c819f222c
tranMinhTien2k3 <21013123@st.phenikaa-uni.edu.vn>
2024-05-17 22:59:26 +0700

    create layout of pages

--- src/router/web.js
+++ src/router/web.js
@@ -4,8 +4,25 @@ const router = express.Router()
 router.get('/', (req, res) => {
     res.render('main');
 });
+router.get('/home', (req, res) => {
+    res.render('main');
+});
 router.get('/login_register', (req, res) => {
     res.render('login_register');
 });
+
+router.get('/introduce', (req, res) => {
+    res.render('intro');
+});
+
+router.get('/location', (req, res) => {
+    res.render('loca');
+});
+
+router.get('/experience', (req, res) => {
+    res.render('exp');
+});
+
+
 
 module.exports = router
